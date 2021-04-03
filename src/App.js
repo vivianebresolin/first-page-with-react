82ec829aa0d0e813d693edaac48921fdf44d134b
vivianebresolin <vivianebresolin@gmail.com>
2021-04-02 22:18:54 -0300

    Added component Header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Routes from './routes';
+import './app.css';
 
 export default function App() {
   return <Routes />
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,26 @@
 import styled from 'styled-components';
+import backgroundImage from '../../images/sea.jpg';
 
 export default function Header() {
+    const Header = styled.header`
+        width: 100%;
+        height: 18em;
+        background: linear-gradient(rgba(0,0,0,.5),rgba(0,0,0,.6)), url(${backgroundImage});
+        background-size: cover;
+        display: flex;
+        align-items: center;
+        justify-content: center;        
+    `;
+
     const Paragraph = styled.p`
-    
-    `
+        color: white;
+        font-size: 1.8rem;
+        font-family: 'Roboto', sans-serif;
+    `;
 
     return (
-        <Paragraph />
+        <Header>
+            <Paragraph>Minha primeira página com React</Paragraph>
+        </Header>
     )
 }
